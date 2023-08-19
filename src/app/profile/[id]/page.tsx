9be5ac53cc0b253bb9b42389a4744eca8a0110be
Abdullah Aziz <abdullahazees4@gmail.com>
2023-08-19 23:52:42 +0500

    logout added

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+import Link from "next/link";
+import axios from "axios";
+
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import { useState } from "react";
+
 export default function userProfile({ params }: any) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const logout = async () => {
+    try {
+      const response = await axios.get("/api/users/logout");
+      toast.success(response.data.message);
+      router.push("/login");
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl">Profile of {params.id}</h1>
+      <button
+        onClick={logout}
+        className="p-2 bg-slate-900 text-white hover:bg-slate-700 cursor-pointer border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        Logout
+      </button>
     </div>
   );
 }
